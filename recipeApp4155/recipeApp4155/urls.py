02cb9b42b63a32d13e04a5e0666be7bd77590bcb
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-02-21 15:37:47 +0000

    homeview

--- recipeApp4155/recipeApp4155/urls.py
+++ recipeApp4155/recipeApp4155/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from app1.views import (
+    home_view,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('catalog/', home_view,),
+    path('', home_view,),
 ]
