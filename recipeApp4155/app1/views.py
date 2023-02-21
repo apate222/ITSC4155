33e7c38fc393bcd55b87ff892b98aca534e8c56c
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-02-21 15:37:25 +0000

    home view

--- recipeApp4155/app1/views.py
+++ recipeApp4155/app1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(request):
+    context = {} 
+    return render(request, "app1/home.html", context)
+
--- recipeApp4155/recipeApp4155/settings.py
+++ recipeApp4155/recipeApp4155/settings.py
@@ -31,13 +31,21 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
+
+    #my apps
+    "app1",
+
+    #django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+
 ]
 
 MIDDLEWARE = [
